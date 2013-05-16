ccfa7b772f931b92964b0e7920af671ae325664e
Iñaki Garay <igarai@gmail.com>
2013-05-16 12:04:13 -0300

    itweep_searcher test

--- test/itweet_SUITE.erl
+++ test/itweet_SUITE.erl
@@ -10,10 +10,10 @@
     , end_per_testcase/2
     ]).
 -export(
-    [ test_itweet/1
-    , test_itweep_searcher/1
+    [ test_itweep/1
+    , test_searcher/1
     , dummy_test/1
-    % , run_eunit/1
+    , run_eunit/1
     ]).
 
 -type config() :: [{atom(), term()}].
@@ -22,9 +22,9 @@
 all() ->
     [
       dummy_test
-     % , run_eunit
-    , test_itweet
-    , test_itweep_searcher
+    , run_eunit
+    , test_itweep
+    , test_searcher
     ].
 
 -spec init_per_suite(config()) -> config().
@@ -32,57 +32,50 @@ init_per_suite(Config) ->
     DD = ?config(data_dir, Config),
     PD = ?config(priv_dir, Config),
     io:format("data: ~p~npriv: ~p~n", [DD, PD]),
-    ServerConfig1 =
-        [ {port,          8080}
-        , {server_name,   "itweet_test_httpd"}
-        , {server_root,   PD}
-        , {document_root, DD}
-        , {bind_address,  "localhost"}
-        ],
-    ServerConfig2 =
-        [ {port,          8080}
-        , {server_name,   "itweet_test_httpd"}
-        , {server_root,   PD}
-        , {document_root, DD}
-        , {bind_address,  "localhost"}
-        ],
-    [ {test_itweet_config,          ServerConfig1}
-    , {test_itweep_searcher_config, ServerConfig2}
-    | Config].
+    ibrowse:start(),
+    [{port, 10000} | Config].
 
 -spec end_per_suite(config()) -> config().
 end_per_suite(Config) ->
     Config.
 
 -spec init_per_testcase(atom(), config()) -> config().
-init_per_testcase(dummy_test, Config) -> Config;
-% init_per_testcase(run_eunit, Config) -> Config;
-init_per_testcase(test_itweet, Config) ->
-    inets:start(),
-    ServerConfig = ?config(test_itweet_config, Config),
-    {ok, Pid} = inets:start(httpd, ServerConfig),
-    [{httpd_pid, Pid} | Config];
-init_per_testcase(test_itweep_searcher, Config) ->
-    inets:start(),
-    ServerConfig = ?config(test_itweep_searcher_config, Config),
-    {ok, Pid} = inets:start(httpd, ServerConfig),
-    [{httpd_pid, Pid} | Config].
+init_per_testcase(dummy_test,  Config) -> Config;
+init_per_testcase(run_eunit,   Config) -> Config;
+init_per_testcase(test_itweep, Config) ->
+    DD   = ?config(data_dir, Config),
+    Port = ?config(port,     Config),
+    MockServer = mock_server:start(
+        [ {port,     Port}
+        , {module,   itweep}
+        , {function, test_itweep}
+        , {data_dir, DD}
+        ]),
+    [ {mock_server, MockServer} | Config];
+init_per_testcase(test_searcher, Config) ->
+    DD   = ?config(data_dir, Config),
+    Port = ?config(port,     Config),
+    MockServer = mock_server:start(
+        [ {port,     Port}
+        , {module,   itweep_searcher}
+        , {function, test_searcher}
+        , {data_dir, DD}
+        ]),
+    [ {mock_server, MockServer} | Config].
 
 -spec end_per_testcase(atom(), config()) -> config().
-end_per_testcase(dummy_test, Config) -> Config;
-% end_per_testcase(run_eunit, Config) -> Config;
-end_per_testcase(test_itweet, Config) ->
-    Pid = ?config(httpd_pid, Config),
-    ok = inets:stop(httpd, Pid),
-    inets:stop(),
+end_per_testcase(dummy_test,  Config) -> Config;
+end_per_testcase(run_eunit,   Config) -> Config;
+end_per_testcase(test_itweep, Config) ->
+    MockServer = ?config(mock_server, Config),
+    mock_server:stop(MockServer),
     Config;
-end_per_testcase(test_itweep_searcher, Config) ->
-    Pid = ?config(httpd_pid, Config),
-    ok = inets:stop(httpd, Pid),
-    inets:stop(),
+end_per_testcase(test_searcher, Config) ->
+    MockServer = ?config(mock_server, Config),
+    mock_server:stop(MockServer),
     Config.
 
-%% Test Cases
+%%% Test Cases
 
 -spec dummy_test(config()) -> _.
 dummy_test(Config) ->
@@ -94,21 +87,52 @@ dummy_test(Config) ->
 % This test case runs the former eunit tests.
 -spec run_eunit(config()) -> _.
 run_eunit(_Config) ->
-    ok = eunit:test(itweep_test),
-    ok = eunit:test(qa_itweep).
+    % ok = eunit:test(itweep_test),
+    % ok = eunit:test(qa_itweep)
+    ok.
 
 % This case tests the itweet functionality.
--spec test_itweet(config()) -> _.
-test_itweet(Config) ->
+-spec test_itweep(config()) -> _.
+test_itweep(Config) ->
     DD = ?config(data_dir, Config),
     PD = ?config(priv_dir, Config),
     io:format("data: ~p~npriv: ~p~n", [DD, PD]),
     ok.
 
 % This case tests the itweep_searcher functionality.
--spec test_itweep_searcher(config()) -> _.
-test_itweep_searcher(Config) ->
-    DD = ?config(data_dir, Config),
-    PD = ?config(priv_dir, Config),
+-spec test_searcher(config()) -> _.
+test_searcher(Config) ->
+    io:format("Starting test: test_searcher.~n"),
+
+    DD     = ?config(data_dir, Config),
+    PD     = ?config(priv_dir, Config),
+    Port   = ?config(port,     Config),
+    application:set_env(itweet, consumer_key,    "iO26pdVUB8AZfJSMWExaBA"),
+    application:set_env(itweet, consumer_secret, "bzze1HOeMYJfVdWQIN1XagMwL4NBegnWFZNd1uGE"),
+    Token  = "520898702-NTU3vKubs0R2QAnJLmXYPxL9bJ3dL8nB0z85KJU",
+    Secret = "997jOo3jECfynlRwmkMMAcQsJsuDh5XL8m7yX3OZ2rM",
     io:format("data: ~p~npriv: ~p~n", [DD, PD]),
+
+    MockServerUrl = "http://localhost:" ++ integer_to_list(Port) ++ "/",
+    io:format("Mock server url: ~s~n", [MockServerUrl]),
+
+    io:format("Starting test server.~n"),
+    try
+        {ok, Server} = itweep_searcher:start(test_searcher_cb, [],
+            [ {token,  Token}
+            , {secret, Secret}
+            , {search_frequency, 800}
+            , {url,    MockServerUrl}
+            ]),
+        io:format("Loading filters.~n"),
+        itweep_searcher:call(Server, {load_filters, "#test"}),
+        io:format("Sleeping.~n"),
+        timer:sleep(30000),
+        io:format("Getting responses.~n"),
+        Statuses = itweep_searcher:get_statuses(Server),
+        lists:map(fun(S) -> io:format("STATUS:~n~p~n~n", [S]) end, Statuses)
+    catch
+      _:E ->
+        io:format("Error: ~p~nStacktrace: ~p", [E, erlang:get_stacktrace()])
+    end,
     ok.
